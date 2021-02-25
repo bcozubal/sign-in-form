3d5ff5a6b7d865d47975e7c145991ffdc3bb4230
bcozubal <bcozubal@gmail.com>
2021-02-25 09:55:10 +0300

    Initial Commit

---
+++ SignInForm/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SignInForm
+//
+//  Created by Barış Özübal on 25.02.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
